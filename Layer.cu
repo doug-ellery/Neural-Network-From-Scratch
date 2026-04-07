2332e72a9fbe9dedd7cedb085977af66f9ee0aec
Doug Ellery <douglas.ellery@gmail.com>
2026-04-06 22:43:21 -0700

    Fixed a stupid class header syntax issue in Layers.h and also added #include <cuda_runtime.h> to Layer.cpp, and naturally made it Layer.cu to use cuda

--- Layer.cu
+++ Layer.cu
@@ -3,24 +3,28 @@
 #include <iostream>
 #include "MyMatrix.h"
 #include <ctime>
+#include <cuda_runtime.h>
 
 Layer::Layer(int nodesThisLayer, int nodesNextLayer){
     //n_in = how many nodes are in our layer, n_out = how many nodes are in the next layer,
-    //ie. how many weights each node needs to have
+    //ie. how many weights each node needs to have. n_out = how many nodes are in the next layer,
+    //and in this design, biases is going to be the biases for the next layer, just how the 
+    //weights are the weights that take us to the next layer, thus there are n_out biases for 
+    //the next layer as each node has one bias
     n_in = nodesThisLayer;
     n_out = nodesNextLayer;
     CUDA_CHECK(cudaMalloc(((void **)&weights, n_in * n_out *sizeof(float)));
-    CUDA_CHECK(cudaMalloc((void **)&biases, n_in * sizeof(float)));
+    CUDA_CHECK(cudaMalloc((void **)&biases, n_out * sizeof(float)));
 
     //biases can be initialized to 0
-    CUDA_CHECK(cudaMemset((void *)biases, 0, n_in * sizeof(float)));
+    CUDA_CHECK(cudaMemset((void *)biases, 0, n_out * sizeof(float)));
 
     //weights cannot, use He normal initialization because we are using ReLU for now as the activation function
     CUDA_CHECK(cudaMemset((void *)weights, 0, n_in * n_out * sizeof(float)));
     int threadsPerBlock, numBlocks;
     getThreadsBlocks(threadsPerBlock, numBlocks, n_in * n_out);
     unsigned long seed = (unsigned long)time(NULL);
-    
+
     //call our He normal kernel
     weightInitializeKernel<<<numBlocks, threadsPerBlock>>>(weights, n_in * n_out, n_in, seed);
 }
--- Layer.h
+++ Layer.h
@@ -1,14 +1,13 @@
 
-
 #ifndef LAYER_H
 #define LAYER_H
 
-class Layer(){
+class Layer{
     float* weights, *biases;
     int n_in, n_out;
     public:
-    Layer(int, int);
-
+        Layer(int, int);
+        ~Layer();
 };
 
 #endif
